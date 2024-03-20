8602083dfb69f28fef872b566e02a973ea995105
AditiM <99026182+itsaditi1999@users.noreply.github.com>
2024-03-20 21:02:12 +0530

    Update aditi.sh

--- script/aditi.sh
+++ script/aditi.sh
@@ -27,45 +27,40 @@ grep -in "TATATATA" wildtype.fna
 #if its a mutant, then save these lines in another file 
 nano mutants.txt
 #pasted the above mutant sequence
-or grep -n "TATATATA" wildtype.fna > mutants.txt
+or grep -in "TATATATA" wildtype.fna > mutants.txt
 
 cat mutants.txt
 
-wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/195/955/GCF_000195955.2_ASM19595v2/GCF_000195955.2_ASM19595v2_genomic.fna.gz
-
-gunzip GCF_000195955.2_ASM19595v2_genomic.fna.gz
-
-grep -A 1000 "NC_000962.3" GCF_000195955.2_ASM19595v2_genomic.fna | grep -v "^--" > prrA.fna 
+curl https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/195/955/GCF_000195955.2_ASM19595v2/GCF_000195955.2_ASM19595v2_cds_from_genomic.fna.gz | gzip -d > prrA_gene.fna 
 
 ls
 
-cat prrA.fna
+cat prrA_gene.fna
 
 ls
-grep -in "TATATA" prrA.fna
+grep -in "TATATA" prrA_gene.fna
 
-(only one line has it)
-
-grep -c "^[ATCGN]" prrA.fna
+grep -c "^[ATCGN]" prrA_gene.fna
+#55145
 
 #to calculate the number of A,T,G,C
-grep -o "A" prrA.fna | wc -l
-grep -o "C" prrA.fna | wc -l
-grep -o "T" prrA.fna | wc -l
-grep -o "G" prrA.fna | wc -l
+grep -o "A" prrA_gene.fna | wc -l
+grep -o "C" prrA_gene.fna | wc -l
+grep -o "T" prrA_gene.fna | wc -l
+grep -o "G" prrA_gene.fna | wc -l
 
 #% GC content
-awk '!/^>/{gc+=gsub(/[gGcC]/,""); at+=gsub(/[aAtT]/,"");} END{ printf "%.2f%%\n", (gc*100)/(gc+at) }' prrA.fna
+awk '!/^>/{gc+=gsub(/[gGcC]/,""); at+=gsub(/[aAtT]/,"");} END{ printf "%.2f%%\n", (gc*100)/(gc+at) }' prrA_gene.fna
 
 touch aditi_nucleotide_file.txt
 
 ls
 
 # to add the number of A,C,T,G in a nucleotide_file.txt
-echo "Number of C: $(grep -o 'C' prrA.fna | wc -l)" >> aditi_nucleotide_file.txt
-echo "Number of A: $(grep -o 'A' prrA.fna | wc -l)" >> aditi_nucleotide_file.txt
-echo "Number of T: $(grep -o 'T' prrA.fna | wc -l)" >> aditi_nucleotide_file.txt
-echo "Number of G: $(grep -o 'G' prrA.fna | wc -l)" >> aditi_nucleotide_file.txt
+echo "Number of C: $(grep -o 'C' prrA_gene.fna | wc -l)" >> aditi_nucleotide_file.txt
+echo "Number of A: $(grep -o 'A' prrA_gene.fna | wc -l)" >> aditi_nucleotide_file.txt
+echo "Number of T: $(grep -o 'T' prrA_gene.fna | wc -l)" >> aditi_nucleotide_file.txt
+echo "Number of G: $(grep -o 'G' prrA_gene.fna | wc -l)" >> aditi_nucleotide_file.txt
 
 cat aditi_nucleotide_file.txt
 
