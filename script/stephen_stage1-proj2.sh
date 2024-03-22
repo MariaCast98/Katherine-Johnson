c6b6d53a2d65fa38486c41e2c8ade5fa0705d44f
stephtromycin <105877415+stephtromycin@users.noreply.github.com>
2024-03-22 23:46:53 +0800

    Update stephen_stage1-proj2.sh

--- script/stephen_stage1-proj2.sh
+++ script/stephen_stage1-proj2.sh
@@ -16,10 +16,10 @@ $ conda install tsnyder::figlet
 
 
 ## 6. Install bwa through the bioconda channel
-$ conda install -c bioconda bwa
+$ conda install bwa
 
 ## 7. Install BLAST through the bioconda channel
-$ conda install -c bioconda blast
+$ conda install blast
 
 
 ## 8. Install samtools through the bioconda channel
@@ -27,16 +27,16 @@ $ conda create -n samtools
 $ conda activate samtools
 $ conda config --add channels bioconda
 $ conda config --add channels conda-forge
-$ conda install -c bioconda samtools
+$ conda install samtools
 
 ## 9. Install bedtools through the bioconda channel
-$ conda install -c bioconda bedtools
+$ conda install bedtools
 
 ## 10. Install spades.py through the bioconda channel
-$ conda install -c bioconda spades
+$ conda install spades
 
 ## 11. Install bcftools through the bioconda channel
-$ conda install -c bioconda bcftools
+$ conda install bcftools
 
 ## 12. Install fastp through the bioconda channel
 $ conda install -c bioconda fastp
