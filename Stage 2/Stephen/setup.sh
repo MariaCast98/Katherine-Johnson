3592861f90e3adcc287098e5569b2f34d0464856
Stephen Kyle Arcan <105877415+stephtromycin@users.noreply.github.com>
2024-04-05 11:49:40 +0800

    Update setup.sh

--- Stage 2/Stephen/setup.sh
+++ Stage 2/Stephen/setup.sh
@@ -1,5 +1,18 @@
-FastQC
-FastP
-bwa
-Samtools
-Freebayes
+##FastQC
+$ sudo apt install fastqc
+
+## Install fastp 
+$ conda install -c bioconda fastp
+
+## Install bwa 
+$ conda install -c bioconda bwa
+
+## Install samtools
+$ conda create -n samtools
+$ conda activate samtools
+$ conda config --add channels bioconda
+$ conda config --add channels conda-forge
+$ conda install -c bioconda samtools
+
+## Freebayes
+$ sudo apt install freebayes
